fbe220fe33d8f7239b753ba705e8a22d159f9c55
Hamed Farag <hamed@wakecap.com>
2025-09-08 14:25:28 +0300

    fix: change api_url key name

--- packages/frontend/src/config/env.ts
+++ packages/frontend/src/config/env.ts
@@ -14,7 +14,7 @@ const config: Config = {
     version: process.env.NEXT_PUBLIC_APP_VERSION || "1.0.0",
   },
   api: {
-    baseUrl: process.env.API_URL || "http://localhost:4009",
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4009",
   },
 };
 
